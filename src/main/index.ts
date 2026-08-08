1119f696cd8e3a38a983b99fd1aa44e393c79ccf
ScorchingSun <1603022925@qq.com>
2026-08-08 15:32:14 +0800

    remove ineffective Autofill disable flag

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,9 +8,6 @@ import { app, BrowserWindow, ipcMain, Tray, Menu, Notification, nativeImage, des
 import * as path from 'path';
 import * as fs from 'fs';
 
-// 禁用 Autofill 避免 DevTools 中的噪音错误
-app.commandLine.appendSwitch('disable-features', 'Autofill');
-
 // 导入服务模块
 import {
   getMessages,
